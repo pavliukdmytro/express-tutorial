44a57e08faa1ba686a041f5da21a33660fbdbefa
pavliuk <Dima>
2020-02-11 17:42:49 +0200

    video 21

--- routes/archive.js
+++ routes/archive.js
@@ -4,35 +4,35 @@ const router = express.Router();
 const config = require('../config');
 const {Post, User} = require('../models');
 
-function posts(req, res) {
+async function posts(req, res) {
     const {userId, userLogin} = req.session;
     const perPage = +config.PER_PAGE;
     const page = req.params.page || 1;
+    try {
+        const posts = await Post.find({}).skip(perPage * page - perPage)
+            .limit(perPage)
+            .populate('owner')
+            .sort({createdAt: -1});
 
-    Post.find({})
-        .skip(perPage * page - perPage)
-        .limit(perPage)
-        .populate('owner')
-        .sort({createdAt: -1})
-        .then(posts => {
-            // console.log(posts);
-            Post.count().then(count => {
-                res.render('archive/index', {
-                    user: {
-                        id: userId,
-                        login: userLogin,
-                    },
-                    posts,
-                    current: page,
-                    pages: Math.ceil(count / perPage)
-                })
-            }).catch(() => {throw new Error('Server Error')});
-        }).catch(() => {throw new Error('Server Error')});
+        const count = await Post.count();
+
+        res.render('archive/index', {
+            user: {
+                id: userId,
+                login: userLogin,
+            },
+            posts,
+            current: page,
+            pages: Math.ceil(count / perPage)
+        })
+    } catch(err) {
+        throw new Error('Server Error')
+    }
 }
 
 router.get("/", posts);
 router.get('/archive/:page', posts);
-router.get('/posts/:post', (req, res, next) => {
+router.get('/posts/:post', async (req, res, next) => {
     const {userId, userLogin} = req.session;
     const url = req.params.post.trim().replace(/  +(?=)/g, ' ');
 
@@ -41,9 +41,8 @@ router.get('/posts/:post', (req, res, next) => {
         err.status = 404;
         next(err);
     } else {
-        Post.findOne({
-            url
-        }).then( post => {
+        try {
+            const post = await Post.findOne({ url });
             if(!post) {
                 const err = new Error('Not found');
                 err.status = 404;
@@ -57,42 +56,41 @@ router.get('/posts/:post', (req, res, next) => {
                     post
                 })
             }
-        })
+        } catch (err) {
+            throw new Error('Server Error');
+        }
     }
 });
 //users posts
-router.get('/users/:login/:page*?', (req,res) => {
+router.get('/users/:login/:page*?', async (req,res) => {
     const {userId, userLogin} = req.session;
     const perPage = +config.PER_PAGE;
     const page = req.params.page || 1;
     const login = req.params.login;
 
-    User.findOne({
-        login
-    }).then(user => {
-        Post.find({owner: user.id})
+    try {
+        const user = await User.findOne({ login });
+
+        const posts = await Post.find({owner: user.id})
             .skip(perPage * page - perPage)
             .limit(perPage)
-            .sort({createdAt: -1})
-            .then(posts => {
-                Post.count({
-                        owner: user.id
-                    }).then(count => {
-                        res.render('archive/user', {
-                            user: {
-                                id: userId,
-                                login: userLogin,
-                            },
-                            posts,
-                            current: page,
-                            pages: Math.ceil(count / perPage),
-                            _user: user
-                        })
-                }).catch(() => {throw new Error('Server Error')});
-            }).catch(() => {throw new Error('Server Error')});
-    })
+            .sort({createdAt: -1});
 
+        const count = await Post.count({ owner: user.id });
 
+        res.render('archive/user', {
+            user: {
+                id: userId,
+                login: userLogin,
+            },
+            posts,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            _user: user
+        });
+    } catch (err) {
+        throw new Error('Server Error');
+    }
 });
 
 module.exports = router;
